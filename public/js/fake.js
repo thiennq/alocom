6fb7a6fb20c68282f2d6dc8f510f30821bdceb23
Tuan Chau <tuanchauict@gmail.com>
2015-06-18 08:40:05 +0700

    add client

---
+++ public/js/fake.js
@@ -0,0 +1,32 @@
+var fakeMenu = {
+  menu: [{
+    id: 0,
+      name: "Cá kho",
+    list: [{
+          fbid: 1010101,
+          fullname: "Thiện Nguyễn",
+          updated_time: new Date()
+      }, {
+        fbid: 202020,
+          fullname: "Phuc Dang",
+          updated_time: new Date()
+      }],
+    total: 2
+  },
+  {
+    id: 1,
+      name: "Thịt kho",
+    list: [],
+      total: 0
+  },
+  {
+    id: 2,
+      name: "Canh chua",
+    list: [{
+          fbid: 3030303,
+          fullname: "Nguyễn Mậu Quang Vũ",
+          updated_time: Date()
+        }],
+    total: 1
+  }
+]};
