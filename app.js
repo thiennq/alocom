11b16d454a74453899d6186f76da07e276885b6d
thiennq <quangthien.cse10@gmail.com>
2018-07-04 01:34:06 +0700

    Update app.js

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ var app = express();
 
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+  res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,PUT,POST,DELETE');
   next();
 });
 
